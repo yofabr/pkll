bb50d64215d40066793f93f9c1c34f4218a75b64
Yoftahe Abraham <jofab08@gmail.com>
2026-04-13 21:37:33 +0300

    chore: Add version displayer

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::env;
 mod helper;
 
 #[derive(Parser, Debug)]
+#[clap(version)]
 struct Args {
     port: u16,
 }
@@ -11,8 +12,7 @@ struct Args {
 fn main() {
     let args = Args::parse();
     let port = args.port;
-    let os = env::consts::OS; 
+    let os = env::consts::OS;
 
     helper::handle(os, port);
-    
 }

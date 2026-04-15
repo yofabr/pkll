939ea6e412e012bb4369e2bda59917ccc6e07f36
Yoftahe Abraham <jofab08@gmail.com>
2026-04-15 16:33:52 +0300

    chore: Init flake.nix

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "pkll - A CLI tool to find and kill processes on a specific port";
+
+  nixConfig = {
+    experimental-features = ["nix-command" "flakes"];
+  };
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, rust-overlay }:
+    let
+      overlays = [ rust-overlay.overlays.default ];
+      pkgs = import nixpkgs { inherit overlays; system = "x86_64-linux"; };
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          rust-bin.stable.latest.default
+          gcc
+          pkg-config
+        ];
+      };
+    };
+}
